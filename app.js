37ab6052aa758850198cf287b04d11d59ee9e019
ColdBlood237 <115800548+ColdBlood237@users.noreply.github.com>
2023-03-03 20:32:53 +0100

    Add functions to populate the display when the user click the digits buttons

--- app.js
+++ app.js
@@ -26,5 +26,19 @@ function operate(operator, a, b) {
     }
 }
 
-console.log("test");
-console.log(operate("*", 6, 3));
+const display = document.getElementById("display");
+console.log(display);
+const keyboard = document.getElementById("keyboard");
+console.log(keyboard);
+const digits = document.querySelectorAll(".digit");
+console.log(digits);
+
+keyboard.addEventListener("click", (e) => {
+    let btnClicked = e.target.textContent;
+    let displayValue = 0;
+    if (!isNaN(btnClicked)) {
+        display.textContent += btnClicked;
+        displayValue = display.textContent;
+        console.log(displayValue);
+    }
+})
